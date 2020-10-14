ec6fe3a0b5ce9e405cf595c2fcc81f9fffc394b4
Paras <Paraspanday@123>
2020-10-14 11:19:13 +0530

    Paras

---
+++ RBLMandateRegWebAPI/Controllers/NPCIUnProcessMandateController.cs
@@ -0,0 +1,29 @@
+﻿using RBLMandateRegWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using static RBLMandateRegWebAPI.Models.NPCIUnProcessMandate;
+
+namespace RBLMandateRegWebAPI.Controllers
+{
+    public class NPCIUnProcessMandateController : ApiController
+    {
+        NPCIUnProcessMandateDAL obj1 = new NPCIUnProcessMandateDAL();
+        [HttpPost]
+        [Route("api/NPCIUnProcessMandate/GetData/")]
+        public Dictionary<string, object> BindDetails(GetDatavalue obj)
+        {
+            try
+            {
+                return obj1.BindGrid(obj);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
