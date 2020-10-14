3d5a0ee13359314a10d375a394f239fd84c3e3c7
Vipul Jain <vipulcrystal@gmail.com>
2020-10-14 12:45:57 +0530

    Vipul CHanges on API

---
+++ RBLMandateRegWebAPI/Models/Back_Office_Mandate_Request/MandateRequestData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RBLMandateRegWebAPI.Models.Back_Office_Mandate_Request
+{
+    public class MandateRequestData
+    {
+        public Int64 MandateId { get; set; }
+        public string Frequency { get; set; }
+        public string DebitType { get; set; }
+        public DateTime DateOnMandate { get; set; }
+        public string Refrence1 { get; set; }
+        public string ScanCount { get; set; }
+        public string PrintCount { get; set; }
+        public string Customer1 { get; set; }
+        public string AmountRupeesInWords { get; set; }
+        public string AmountRupees { get; set; }
+        public string BankName { get; set; }
+        public string AcNo { get; set; }
+        public string IFSC { get; set; }
+        public string ToDebit { get; set; }
+        public string MICR { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string UtilityCode { get; set; }
+        public string SponsorbankCode { get; set; }
+        public string RejectedReason { get; set; }
+        public string MandateMode { get; set; }
+        public string MandateType { get; set; }
+        public string UMRN { get; set; }
+        public string CorporateName { get; set; }
+        public string NPCIMsgId { get; set; }
+    }
+}
